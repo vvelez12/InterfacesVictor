5a081725ab0b57c1d261d8b6a08aae139bdd5339
vvelez12 <128139051+vvelez12@users.noreply.github.com>
2024-02-20 20:28:26 +0100

    Add files via upload

---
+++ VíctorVélezPérez_Practica5/VíctorVélez_Practica5.js
@@ -0,0 +1,16 @@
+ // Función para mostrar las instrucciones
+  function mostrar() {
+    const contenedor = document.getElementById('contenedorInstrucciones');
+    contenedor.style.display = 'flex';
+  }
+
+  // Función para cerrar las instrucciones
+  function cerrar() {
+    const contenedor = document.getElementById('contenedorInstrucciones');
+    contenedor.style.display = 'none';
+  }
+
+// Función para cerrar iniciar el juego
+  function iniciarJuego() {
+
+  }
